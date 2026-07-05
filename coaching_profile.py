46af810f4d10c0b5a5b400f2e6419bc97d1fb45e
revo2wheels <44225520+revo2wheels@users.noreply.github.com>
2026-07-05 14:16:13 +0200

    modified:   coaching_profile.py

--- coaching_profile.py
+++ coaching_profile.py
@@ -100,6 +100,7 @@ REPORT_CONTRACT = {
 
         # 🔮 CURRENT PLAN CONTEXT
         "current_ISO_weekly_microcycle",
+        "planned_summary_by_iso_week",
         "future_forecast",
         "future_actions"
     ],
